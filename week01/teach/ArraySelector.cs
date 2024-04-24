2885a9a055a2866bc0c1fe3ab1c57deb7d8b2469
wallacejoe <JAWcoder@gmail.com>
2024-04-24 07:08:50 -0700

    W01 Individual Activity

--- week01/teach/ArraySelector.cs
+++ week01/teach/ArraySelector.cs
@@ -11,6 +11,17 @@ public static class ArraySelector
 
     private static int[] ListSelector(int[] list1, int[] list2, int[] select)
     {
-        return new int[0];
+        int[] result = new int[select.Length];
+        int iterator1 = 0;
+        int iterator2 = 0;
+        for (int i = 0; i < select.Length; i++) {
+            if (select[i] == 1) {
+                result[i] = list1[iterator1++];
+            }
+            else {
+                result[i] = list2[iterator2++];
+            }
+        }
+        return result;
     }
 }
